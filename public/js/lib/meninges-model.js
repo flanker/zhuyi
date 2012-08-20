7ce8e01d3d9370ba949ac072b003e7d14515b723
FENG Zhichao <flankerfc@gmail.com>
2012-08-21 00:22:53 +1000

    add meninges javascript labrary

---
+++ public/js/lib/meninges-model.js
@@ -0,0 +1,110 @@
+Backbone.MeningesModel = Backbone.Model.extend({
+
+  constructor: function() {
+    Backbone.Model.prototype.constructor.apply(this, arguments);
+    this.replaceWithMeningesAttributes(this.attributes);
+  },
+
+  toJSON: function () {
+    var o = Backbone.Model.prototype.toJSON.apply(this, arguments);
+    var self = this;
+    if (this.associations) {
+      _(_(this.associations).keys()).each(function (key) {
+        var obj = self.lookupConstructor(self.associations[key].model);
+        if (obj !== undefined && o[key]) {
+          o[key] = o[key].toJSON();
+        }
+      });
+    }
+    return o;
+  },
+
+  parse: function (attrs, xhr, isNested) {
+    var attrsClone = _(attrs).clone();
+    this.replaceWithMeningesAttributes(attrs);
+    if (isNested) {
+      this.removeAttributesNotProvided(attrsClone);
+    }
+    return attrs;
+  },
+
+  removeAttributesNotProvided: function (attrs) {
+    var keys = _(this.attributes).keys();
+    var self = this;
+    _(keys).each(function (key) {
+      if (!attrs[key]) {
+        self.unset(key);
+      }
+    });
+  },
+
+  clone: function() {
+    return new this.constructor(this.toJSON());
+  },
+
+  replaceWithMeningesAttributes: function (attrs) {
+    if (this.associations) {
+      var self = this;
+      var associationsForAttrs = _(_(this.associations).keys()).intersection(_(attrs).keys());
+      _(associationsForAttrs).each(function (key) {
+        var obj = self.lookupConstructor(self.associations[key].model);
+        if (obj !== undefined) {
+          if (self.get(key) && self.get(key) instanceof Backbone.Model) {
+            self.get(key).set(self.get(key).parse(attrs[key], null, true));
+            delete attrs[key];
+          }
+          else if (self.isKeyAnUpdatableCollection(self, key, attrs)) {
+            self.populateCollectionFromArray(attrs[key], self.get(key));
+            delete attrs[key];
+          }
+          else if (attrs && attrs[key]) {
+            attrs[key] = new obj(attrs[key]);
+          }
+        }
+      });
+    }
+  },
+
+  isKeyAnUpdatableCollection: function (model, key, attrs) {
+    return model.get(key) && (model.get(key) instanceof Backbone.Collection) && attrs && attrs[key]
+  },
+
+  populateCollectionFromArray: function (els, collection) {
+    var modelsToRemove = [];
+    var indexesToRemove = [];
+    collection.each(function (model) {
+      var matched = false;
+      _(els).each(function (el, index) {
+        if (model.equals && model.equals(el)) {
+          model.set(model.parse(el, null, true));
+          matched = true;
+          indexesToRemove.push(index);
+        }
+      });
+      if (!matched) {
+        modelsToRemove.push(model);
+      }
+    });
+
+    _(modelsToRemove).each(function (model) {
+      collection.remove(model);
+    });
+    _(indexesToRemove).each(function (index) {
+      delete els[index];
+    });
+
+    _(els).each(function (el) {
+      if (el) {
+        collection.add(new collection.model(el));
+      }
+    });
+  },
+
+  lookupConstructor: function (classPath) {
+    var obj = window;
+    _(classPath.split(".")).each(function(pathElement) {
+      obj = obj[pathElement];
+    });
+    return obj;
+  }
+});
---
+++ public/js/lib/meninges-view.js
@@ -0,0 +1,116 @@
+Backbone.MeningesView = {
+  extend: function (o) {
+
+    var coerceType = function (originalValue, value) {
+        if (_(originalValue).isNumber()) {
+          var parsedValue = parseFloat(value);
+          value = _(parsedValue).isNaN() ? value : parsedValue;
+          value = value === '' ? null : value;
+        }
+        if (value === '' && originalValue === null) {
+          value = null;
+        }
+        return value;
+    }
+
+    var findNextModel = function (startingModel, pathElement) {
+      if (pathElement.indexOf(":") === -1) {
+        return startingModel.get(pathElement);
+      }
+      else {
+        var a = pathElement.split(":");
+        return startingModel.get(a[0]).at(parseInt(a[1]));
+      }
+    };
+
+    var extractValue = function (element) {
+      if (element.type === "checkbox") {
+        return element.checked;
+      }
+      if (element.type === "radio") {
+        return $("input[name='" + element.name + "']:checked").val();
+      }
+      return element.value || '';
+    };
+
+    o.events = o.events || {};
+    o.events["blur .meninges"] = 'updateAttributeForEvent';
+    o.events["change input.meninges[type='checkbox']"] = 'updateAttributeForEvent';
+    o.events["change input.meninges[type='radio']"] = 'updateAttributeForEvent';
+    o.events["change select.meninges"] = 'updateAttributeForEvent';
+
+    o.updateAttribute = function(pathItems, value) {
+      var currentModel = this.model;
+      var originalModel = this._originalModel;
+      for (var i = 0; i < pathItems.length - 1; i++) {
+        currentModel = findNextModel(currentModel, pathItems[i]);
+        originalModel = findNextModel(originalModel, pathItems[i]);
+      }
+
+      var key = _(pathItems).last();
+
+      if (key.indexOf(":") === -1) {
+        var newValueHash = {};
+        var oldValue = currentModel.get(key);
+        var originalValue = originalModel ? originalModel.get(_(pathItems).last()) : null;
+        var value = coerceType(originalValue, value);
+
+        var sameValue = (oldValue === value);
+        newValueHash[_(pathItems).last()] = value;
+        if (!sameValue) {
+          currentModel.set(newValueHash);
+        }
+      } else {
+        var elements = key.split(":");
+        var collection = currentModel.get(elements[0]);
+        var index = elements[1];
+        collection[index] = coerceType(collection[index], value);
+      }
+    };
+
+    o.updateAttributeForEvent = function (event) {
+      var pathItems = event.target.name.split(".");
+      var value = extractValue(event.target);
+      o.updateAttribute.call(this, pathItems, value)
+      event.stopImmediatePropagation();
+    };
+
+    var render = o.render;
+    o.render = function () {
+      this._originalModel = this.model.clone();
+      render.call(this);
+      return this;
+    };
+
+    var initialize = o.initialize;
+    o.initialize = function () {
+      initialize && initialize.call(this);
+      this._originalModel = this.model.clone();
+      var fetch = this.model.fetch;
+      var save = this.model.save;
+      var that = this;
+      this.model.fetch = function (options) {
+        options = options || {};
+        var onSuccess = options.success;
+        options.success = function (model, resp, options) {
+          that._originalModel = model.clone();
+          onSuccess && onSuccess(model, resp, options);
+        };
+
+        fetch.call(that.model, options);
+      }
+      this.model.save = function (attrs, options) {
+        options = options || {};
+        var onSuccess = options.success;
+        options.success = function (model, resp, options) {
+          that._originalModel = model.clone();
+          onSuccess && onSuccess(model, resp, options);
+        };
+
+        save.call(that.model, attrs, options);
+      }
+    };
+
+    return Backbone.View.extend(o);
+  }
+};
