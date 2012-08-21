a5eed9ce9f95478c3107d75f1941f0f11123c76f
FENG Zhichao <flankerfc@gmail.com>
2012-08-21 13:09:52 +1000

    add fadeOut fadeIn effect then route changes

--- public/js/application.js
+++ public/js/application.js
@@ -12,27 +12,43 @@ App.router.AppRouter = Backbone.Router.extend({
   },
 
   newRequest: function () {
-    var accommodation = new App.models.Accommodation();
-    accommodation.fetch({
-      success: function (accommodation) {
-        var newView = new App.views.NewView({model: accommodation}).el;
-        $('.main-container').html(newView);
-      }
+    var self = this;
+    self.fadeOutMain(function () {
+      new App.models.Accommodation().fetch({
+        success: function (accommodation) {
+          self.fadeInMain(new App.views.NewView({model: accommodation}).el);
+        }
+      });
     });
   },
 
   recent: function () {
-    var accommodations = new App.models.Accommodations();
-    accommodations.fetch({
-      success: function (accommodations) {
-        var recentView = new App.views.RecentView({collection: accommodations}).el;
-        $('.main-container').html(recentView);
-      }
+    var self = this;
+    self.fadeOutMain(function () {
+      new App.models.Accommodations().fetch({
+        success: function (accommodations) {
+          self.fadeInMain(new App.views.RecentView({collection: accommodations}).el);
+        }
+      });
     });
   },
 
   empty: function () {
     App.app.navigate('!/new', {trigger: true});
+  },
+
+  fadeOutMain: function (callback) {
+    var $mainContainer = $('.main-container');
+
+    $mainContainer.fadeOut(function () {
+      callback($mainContainer);
+    });
+  },
+
+  fadeInMain: function (view) {
+    var $mainContainer = $('.main-container');
+    $mainContainer.html(view);
+    $mainContainer.fadeIn();
   }
 
 });
