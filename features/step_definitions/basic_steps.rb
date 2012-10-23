10c02225d1144b0f1b8a556592bdc69ce9e57010
FENG Zhichao <flankerfc@gmail.com>
2012-10-23 14:16:44 +1100

    fix the cucumber steps

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -3,7 +3,21 @@ When /^I am on the new request page$/ do
 end
 
 When /^I fill following fields:$/ do |table|
-  # table is a Cucumber::Ast::Table
+  table.rows_hash.each_pair do |name, value|
+    label = find 'label', :text => name
+    field = find "[name='#{label[:for]}']"
+
+    case field[:type]
+    when 'select_one'
+      select value, :from => name
+    when 'radio'
+      choose value
+    when 'checkbox'
+      check name
+    when 'text'
+      fill_in name, :with => value
+    end
+  end
 end
 
 When /^I save the request$/ do
